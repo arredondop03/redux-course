ea4fd9a98dac95f899eadc38fb64659b406f0fb6
Paola Arredondo <arredondop03@gmail.com>
2018-10-16 21:10:19 -0400

    Provider installed

--- src/index.js
+++ src/index.js
@@ -9,10 +9,5 @@ import reducer from './store/reducer';
 
 const store = createStore(reducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider> <App /> </Provider>, document.getElementById('root'));
 registerServiceWorker();
-
-
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-///////////////reduxm -09-
